e1866feac317959f2a78c5c5153a6c26c9ced674
depth221 <depth221@gmail.com>
2016-10-15 12:11:44 +0900

    Delete temp.py

--- temp.py
+++
@@ -1,8 +0,0 @@
-while(1):
-    try:
-        temp_f = input("Enter temp: ")
-        temp_c = (float(temp_f) - 32) * (5/9) 
-        
-        print(temp_f, "℉ =", temp_c, "℃")
-    except:
-        print("Please enter a number")
